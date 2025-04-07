78446680336653383042585945468b28f66a4977
anilkumarkari <37361124+anilkumarkari@users.noreply.github.com>
2025-04-07 14:43:57 +0800

    Create variables.tf

---
+++ modules/vnet/variables.tf
@@ -0,0 +1,24 @@
+variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
+}
+
+variable "address_space" {
+  description = "The address space for the virtual network"
+  type        = list(string)
+}
+
+variable "subnets" {
+  description = "A map of subnets with subnet names as keys and address prefixes as values"
+  type        = map(string)
+}
+
+variable "location" {
+  description = "The location of the virtual network"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
