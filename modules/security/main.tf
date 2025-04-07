df124cb1efbcf8ad7d2c3c1882b023f791a3b38b
anilkumarkari <37361124+anilkumarkari@users.noreply.github.com>
2025-04-07 14:44:54 +0800

    Update main.tf

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,1 +1,10 @@
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-${var.environment}"
+  location            = var.location
+  resource_group_name = module.network.resource_group_name
+}
+
+output "nsg_name" {
+  value = azurerm_network_security_group.nsg.name
+}
 
