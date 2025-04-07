15818cc03cdafdccac9eef9498bfd840829f0759
anilkumarkari <37361124+anilkumarkari@users.noreply.github.com>
2025-04-07 14:45:18 +0800

    Update main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,1 +1,11 @@
+resource "azurerm_storage_account" "storage" {
+  name                     = "st${var.environment}storageacct"
+  resource_group_name       = module.network.resource_group_name
+  location                 = var.location
+  account_tier              = "Standard"
+  account_replication_type = "LRS"
+}
 
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
